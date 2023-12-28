5d943b7366186ee3c16f68c0738b2f86d632bab9
BrianQu <qujianning0401@163.com>
2023-12-27 20:25:56 -0500

    feature: LC739 daily temperature

--- stack/src/main/java/daily_temperature_739.java
+++ stack/src/main/java/daily_temperature_739.java
@@ -7,6 +7,13 @@ import java.util.Stack;
  * @since 2023-12-27
  */
 public class daily_temperature_739 {
+    /**
+     * @implSpec Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+     * @author Brian Qu
+     * @param temperatures an array of integers temperatures represents the daily temperatures
+     * @return int[] - answer[i] is the number of days you have to wait after the ith day to get a warmer temperature
+     * @since 2023-12-27 20:22
+     */
     public int[] dailyTemperatures(int[] temperatures) {
         // initialization
         int n = temperatures.length;
