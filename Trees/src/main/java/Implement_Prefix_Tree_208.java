abd57ca6d21959d0f7aaf149e9df0c25e956c18b
BrianQu <qujianning0401@163.com>
2024-01-11 19:18:44 -0500

    feature: LC208 Implement Prefix Tree

---
+++ Trees/src/main/java/Implement_Prefix_Tree_208.java
@@ -0,0 +1,62 @@
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec None
+ * @since 2024-01-11
+ */
+public class Implement_Prefix_Tree_208 {
+    // Define TrieNode
+    static class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    public Implement_Prefix_Tree_208() {
+        root = new TrieNode();
+    }
+
+    // Insert a word into the trie
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                node.children[ch - 'a'] = new TrieNode();
+            }
+            node = node.children[ch - 'a'];
+        }
+        node.isEndOfWord = true;
+    }
+
+    // search for a word in the trie
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+
+        return node.isEndOfWord;
+    }
+
+    // Check if any word in the trie starts with the given prefix
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+
+        return true;
+    }
+}
