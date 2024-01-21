7376d41bc8ab3513ea38e45a4d95761d0d714d5f
BrianQu <qujianning0401@163.com>
2024-01-20 22:53:07 -0500

    feature: LC684 Redundant Connections

---
+++ Graphs/src/main/java/Redundant_Connections_684.java
@@ -0,0 +1,18 @@
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec None
+ * @since 2024-01-20
+ */
+public class Redundant_Connections_684 {
+    /**
+     * @param
+     * @return
+     * @implSpec In this problem, a tree is an undirected graph that is connected and has no cycles.
+     * You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+     * Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+     * @author Brian Qu
+     * @since 2024-01-20 22:41
+     */
+
+}
