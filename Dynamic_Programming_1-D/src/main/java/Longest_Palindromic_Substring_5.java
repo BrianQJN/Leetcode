432ddc4fc2e3918bfd03308515ad157d84535556
BrianQu <qujianning0401@163.com>
2024-02-08 17:53:00 -0500

    feature: LC5 Longest Palindromic Substring

---
+++ Dynamic_Programming_1-D/src/main/java/Longest_Palindromic_Substring_5.java
@@ -0,0 +1,18 @@
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec None
+ * @since 2024-02-08
+ */
+public class Longest_Palindromic_Substring_5 {
+    /**
+     * @param
+     * @return
+     * @implSpec Given a string s, return the longest palindromic substring in s.
+     * @author Brian Qu
+     * @since 2024-02-08 17:51
+     */
+    public String longestPalindrome(String s) {
+
+    }
+}
