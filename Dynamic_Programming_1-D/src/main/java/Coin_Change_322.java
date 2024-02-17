d2eeafc6b6fbbe41374bb5df7c2095bf593a9f51
BrianQu <qujianning0401@163.com>
2024-02-17 14:32:16 -0500

    feature: LC322 Coin Change

--- Dynamic_Programming_1-D/src/main/java/Coin_Change_322.java
+++ Dynamic_Programming_1-D/src/main/java/Coin_Change_322.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author Brian Qu
  * @version 1.0
@@ -16,5 +18,19 @@ public class Coin_Change_322 {
      * @since 2024-02-12 14:14
      */
     public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[max];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin: coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
