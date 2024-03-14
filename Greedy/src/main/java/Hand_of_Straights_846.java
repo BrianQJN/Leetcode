a9b0e21e5fb71ab8607d80c48bd8b7d5c121533a
BrianQu <qujianning0401@163.com>
2024-03-14 16:51:43 -0400

    feature: LC846 Hand of Straights

--- Greedy/src/main/java/Hand_of_Straights_846.java
+++ Greedy/src/main/java/Hand_of_Straights_846.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author Brian Qu
  * @version 1.0
@@ -15,10 +19,34 @@ public class Hand_of_Straights_846 {
      * @since 2024-03-12 18:26
      */
     public boolean isNStraightHand(int[] hand, int groupSize) {
+        // if the length of hand is not divisible of groupSize, we can't divide into k group
         if (hand.length % groupSize != 0) {
             return false;
         }
 
+        // count the number of each card
+        Map<Integer, Integer> cardCounts = new TreeMap<>();
+        for (int card : hand) {
+            cardCounts.put(card, cardCounts.getOrDefault(card, 0) + 1);
+        }
 
+        // try to regroup the cards
+        for (int card : cardCounts.keySet()) {
+            int count = cardCounts.get(card);
+            if (count > 0) {
+                // try to group based on current card
+                for (int i = 1; i < groupSize; i++) {
+                    int nextCard = card + i;
+                    // check if we can regroup
+                    if (!cardCounts.containsKey(nextCard) || cardCounts.get(nextCard) < count) {
+                        // we don't have enough next card for regroup
+                        return false;
+                    }
+                    cardCounts.put(nextCard, cardCounts.get(nextCard) - count);
+                }
+            }
+        }
+
+        return true;
     }
 }
